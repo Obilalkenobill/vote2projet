53033876ba2bea8eff818fc5dd4b2b8af035bd3a
Obilalkenobill <50761645+Obilalkenobill@users.noreply.github.com>
2022-03-03 07:38:08 +0100

    Update RolePersRepository.php

--- src/Repository/RolePersRepository.php
+++ src/Repository/RolePersRepository.php
@@ -20,72 +20,37 @@ class RolePersRepository extends ServiceEntityRepository
         parent::__construct($registry, RolePers::class);
     }
 
-    // /**
-    //  * @return RolePers[] Returns an array of RolePers objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('r.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?RolePers
-    {
-        return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
-    public function deleteRoleUser(RolePers $role_pers): array
+    public function deleteRoleUser(RolePers $role_pers)
     {
         $conn = $this->getEntityManager()->getConnection();
         $personne_id=$role_pers->getPersonneId()->getId();
         $role_id=$role_pers->getRoleId()->getId();
         $sql = '
           DELETE FROM rolepers WHERE personne_id='.$personne_id.' and role_id='.$role_id;
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-
         // returns an array of arrays (i.e. a raw data set)
-        return $stmt->fetchAllAssociative();
+        return $conn->fetchAllAssociative($sql);
     }
-    public function deleteAboutUser(Personne $personne): array
+    public function deleteAboutUser(Personne $personne)
     {
         $conn = $this->getEntityManager()->getConnection();
         $personne_id=$personne->getId();
         $sql = '
           DELETE FROM rolepers WHERE personne_id='.$personne_id.';DELETE FROM vote where personne_id='.$personne_id.';DELETE FROM Follow where personne_id_id='.$personne_id;
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        $stmt->closeCursor();
+          $conn->fetchAllAssociative($sql);
         $sql2='DELETE FROM projet WHERE personne_id_id='.$personne_id;
-        $stmt2 = $conn->prepare($sql2);
-        $stmt2->execute();
+        $result=$conn->fetchAllAssociative($sql2);
         // returns an array of arrays (i.e. a raw data set)
-        return $stmt->fetchAllAssociative();
+        return $result;
     }
-    public function deleteRole(RolePers $role_pers): array
+    public function deleteRole(RolePers $role_pers)
     {
         $conn = $this->getEntityManager()->getConnection();
         $role_id=$role_pers->getRoleId()->getId();
         $sql = '
           DELETE FROM rolepers WHERE role_id='.$role_id.';DELETE FROM role WHERE id='.$role_id;
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
+          $result= $conn->fetchAllAssociative($sql);
 
         // returns an array of arrays (i.e. a raw data set)
-        return $stmt->fetchAllAssociative();
+        return $result;
     }
 }
