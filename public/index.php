417dcafb81ee8bb4ce92bcd1e652aba48ef1927a
Obilalkenobill <50761645+Obilalkenobill@users.noreply.github.com>
2022-03-23 16:26:29 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -8,6 +8,19 @@ use Symfony\Component\HttpFoundation\Request;
 require dirname(__DIR__).'/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+if ($_SERVER['APP_DEBUG']) {
+    header('Access-Control-Allow-Origin:'.rtrim($_SERVER['HTTP_REFERER'], '/'));
+} else {
+    header('Access-Control-Allow-Origin: https://gestion2votefront.herokuapp.com/');
+    header('Access-Control-Allow-Origin: http://gestion2votefront.herokuapp.com/');
+}
+header('Access-Control-Allow-Headers:*');
+header('Access-Control-Allow-Credentials:true');
+header('Access-Control-Allow-Headers:*');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    die();
+}
+
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
