8631d060c3b4dba864b214623fb2b7e3848aeee7
Obilalkenobill <50761645+Obilalkenobill@users.noreply.github.com>
2022-03-23 17:26:12 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ require dirname(__DIR__).'/vendor/autoload.php';
 if ($_SERVER['APP_DEBUG']) {
     header('Access-Control-Allow-Origin:'.rtrim($_SERVER['HTTP_REFERER'], '/'));
 } else {
-    header('Access-Control-Allow-Origin: https://ricbe.herokuapp.com,http://ricbe.herokuapp.com');
+    header('Access-Control-Allow-Origin:*');
 }
 header('Access-Control-Allow-Headers:*');
 header('Access-Control-Allow-Credentials:true');
